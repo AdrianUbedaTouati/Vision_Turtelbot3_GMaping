628680cda3259a8a1db6c66fe609ed6551c35824
pinguinito <aut3@alu.ua.es>
2023-05-10 17:31:47 +0200

    Añadiendo base

---
+++ Ejemplo/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pinguinito/Escritorio/VAR/Ejemplo/catkin_ws/src
+# Build directory: /home/pinguinito/Escritorio/VAR/Ejemplo/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
